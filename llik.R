35c4bed9f4400ef2aea2fe46f83bab0406be4c6a
jcarlen <jac@Janes-MacBook-Pro.local>
2017-11-12 18:27:58 -0800

    fixed llik.R file. Removed broken multidimensional SRE stuff.

--- llik.R
+++ llik.R
@@ -10,7 +10,7 @@
 library(extraDistr) # for divnchisq
 library(gtools) #for logit function
 #----------------------------------------------------------------------------------------------------
-# 1. llik, llik2 ----------------------------------------------------------------------------------------------------
+# 1. exact: llik, llik2 ----------------------------------------------------------------------------------------------------
 #   assumes no self loops
 #   (for weighting?) add a constant to the data matrix (Y) if it has zeros (should be >=1 so no neg weights)
 #       this was found to be advantageous for residuals anyway
@@ -41,19 +41,9 @@ llik <- function(object=NULL, Y=NULL, sender=NULL, receiver=NULL, beta=NULL,
   
   Z_dist = as.matrix(dist(Z, upper = T))
   
-  if (length(sender) == N) {
-    #l_lambda = t(receiver + t(sender - Z_dist)) + beta;
-    l_lambda = beta + outer(sender, receiver, "+") - Z_dist #slightly faster
-    
-  } else {
-    sr = lapply(1: (length(sender)/N), function(x) {
-      outer(sender[ (N*(x-1)+1) : (N*(x))], receiver[(N*(x-1)+1) : (N*(x))], 
-          function(s,r) {(s+r)^2})
-      })
-    sr = sqrt(Reduce ("+", sr))
-    l_lambda = beta + sr - Z_dist
-  }
-  
+  #l_lambda = t(receiver + t(sender - Z_dist)) + beta;
+  l_lambda = beta + outer(sender, receiver, "+") - Z_dist #slightly faster
+
   if (family == "poisson") {
     lgamma.constant = sum(lgamma(as.vector(Y+1)), na.rm = T) 
     lambda = exp(l_lambda); diag(lambda) = 0
@@ -88,17 +78,14 @@ llik <- function(object=NULL, Y=NULL, sender=NULL, receiver=NULL, beta=NULL,
 llik2 <- function(theta, Y, d, est = "MAP", family = "poisson") {
   
   n = nrow(Y)
-  nparam = length(theta)
   
   B = theta[1]
   Z = matrix(theta[2:(1+d*n)], ncol = d, nrow = n)
-  #split whatever's left
-  a = theta[(2+d*n): (1+d*n/2 + (nparam-1)/2) ]
-  b = theta[(2+d*n/2 + (nparam-1)/2) : nparam]
-  
-  d2 = length(a)/n
+  a = theta[(2+d*n) : (n+1+d*n)]
+  b = theta[(n+2+d*n) : (n*(d+2)+1)]
   
   if (est == "MAP") {
+    nparam = length(theta)
     sigma2_a = theta[nparam-2]
     sigma2_b = theta[nparam-1]
     sigma2_z = theta[nparam]
@@ -106,8 +93,8 @@ llik2 <- function(theta, Y, d, est = "MAP", family = "poisson") {
   
   #exact update conditioned on graph parameters
   if (est == "MAPe") {
-    sigma2_a = (sum(a^2) + sender.var.df*prior.sender.var^2) / (n*d2 + 2 + prior.sender.var)
-    sigma2_b = (sum(b^2) + receiver.var.df*prior.receiver.var^2) / (n*d2 + 2 + prior.receiver.var)
+    sigma2_a = (sum(a^2) + sender.var.df*prior.sender.var^2) / (n + 2 + prior.sender.var)
+    sigma2_b = (sum(b^2) + receiver.var.df*prior.receiver.var^2) / (n + 2 + prior.receiver.var)
     sigma2_z = (sum(Z^2) + Z.var.df*prior.Z.var^2) / (n*d + 2 + prior.Z.var)
   } 
 
@@ -119,7 +106,7 @@ llik2 <- function(theta, Y, d, est = "MAP", family = "poisson") {
 }
 
 #----------------------------------------------------------------------------------------------------
-# 2. llik_hat, llik2_hat  -----------------------------------------------------------------------------------------------
+# 2. approximate: llik_hat, llik2_hat  -----------------------------------------------------------------------------------------------
 #    add a constant to the data matrix (Y) if it has zeros (should be >=1 so no neg weights)
 #       this was found to be advantageous for residuals anyway
 #    faster to calculate dist (uses c) than call it pair by pair or do pair by pair in R. 
@@ -323,17 +310,14 @@ llik_hat2 <- function(theta, Y, d, est = "Y", family = "poisson",
 llik_gr <- function(theta, Y, d, est = "MAP") {
   
   n = nrow(Y)
-  nparam = length(theta)
-  
+
   B = theta[1]
   Z = matrix(theta[2:(1+d*n)], ncol = d, nrow = n)
-  #split whatever's left
-  a = theta[(2+d*n): (1+d*n/2 + (nparam-1)/2) ]
-  b = theta[(2+d*n/2 + (nparam-1)/2) : nparam]
-  
-  d2 = length(a)/n
+  a = theta[(2+d*n): (1+n+d*n)]
+  b = theta[(2+n+d*n) : (n*(d+2) + 1)]
   
   if (est == "MAP") {
+    nparam = length(theta)
     sigma2_a = theta[nparam-2]
     sigma2_b = theta[nparam-1]
     sigma2_z = theta[nparam]
@@ -342,46 +326,19 @@ llik_gr <- function(theta, Y, d, est = "MAP") {
   #exact update of variance parameters conditioned on graph parameters
   if (est == "MAPe") {
     #optimal values
-    sigma2_a = (sum(a^2) + sender.var.df*prior.sender.var^2) / (n*d2 + 2 + prior.sender.var)
-    sigma2_b = (sum(b^2) + receiver.var.df*prior.receiver.var^2) / (n*d2 + 2 + prior.receiver.var)
+    sigma2_a = (sum(a^2) + sender.var.df*prior.sender.var^2) / (n + 2 + prior.sender.var)
+    sigma2_b = (sum(b^2) + receiver.var.df*prior.receiver.var^2) / (n + 2 + prior.receiver.var)
     sigma2_z = (sum(Z^2) + Z.var.df*prior.Z.var^2) / (n*d + 2 + prior.Z.var)
   }
   
 
-  if (length(a == N) ) {
-
-    #lambda = exp(t(b + t(a - Z_dist)) + B); diag(lambda) = 0
-    lambda = B + outer(a, b, "+") - Z_dist; diag(lambda) = 0 #slightly faster
-    
-    da = rowSums(Y) - rowSums(lambda)
-    
-    db = colSums(Y) - colSums(lambda)
-    
-  } else {
-    
-    sr2 = lapply(1: (length(a)/N), function(x) {
-      outer(a[ (N*(x-1)+1) : (N*(x))], b[(N*(x-1)+1) : (N*(x))], "+")
-    })
-    
-    sr = sqrt(Reduce ("+", lapply(sr2, "^", 2)))
-    sr_inv = 1/sr; diag(sr_inv) = rep(0, N)
-    
-    lambda = exp(B + sr - Z_dist); diag(lambda) = 0
-    
-    da = unlist(lapply(sr2, function(x) {rowSums(x*sr_inv*(Y - lambda))}))
-      
-    db = 
-      #l_lambda = t(receiver + t(sender - Z_dist)) + beta;
-      l_lambda = B + outer(sender, receiver, "+") - Z_dist #slightly faster
-      
-    } else {
-      
-      
-      l_lambda = 
-  }
+  #lambda = exp(t(b + t(a - Z_dist)) + B); diag(lambda) = 0
+  Z_dist = dist2(Z)
+  lambda = exp(B + outer(a, b, "+") - Z_dist); diag(lambda) = 0 #slightly faster
+  da = rowSums(Y) - rowSums(lambda)
+  db = colSums(Y) - colSums(lambda)
   
   #add sampling?
-  Z_dist = dist2(Z)
   dist_inv = 1/Z_dist; diag(dist_inv) = rep(0, N) #term1 = cbind(term1, term1)
   tmp1 = (Y + t(Y)) * dist_inv
   tmp2 = dist_inv * (lambda + t(lambda))
@@ -391,7 +348,7 @@ llik_gr <- function(theta, Y, d, est = "MAP") {
   
   #return
   if (est == "Y") {
-    return(c(sum(Y - lambda),
+    return(c(sum(da), #sum(Y - lambda)
          tmp3,
          da,
          db))
@@ -399,7 +356,7 @@ llik_gr <- function(theta, Y, d, est = "MAP") {
   
   if (est == "MAP" ) {
     
-    return(c(-B/beta.var + sum(Y - lambda),
+    return(c(-B/beta.var + sum(da), #sum(Y - lambda)
              
              tmp3 - as.vector(Z)/sigma2_z, # d x N
              
@@ -418,7 +375,7 @@ llik_gr <- function(theta, Y, d, est = "MAP") {
   }
   
   if (est == "MAPe") {
-    return(c(-B/beta.var + sum(Y - lambda),
+    return(c(-B/beta.var + sum(da), #sum(Y - lambda)
              
              tmp3 - as.vector(Z)/sigma2_z, # d x N
              
@@ -505,212 +462,8 @@ llik_gr_hat <- function(theta, Y, d, est = "MAP",
   
 }
 
-#----------------------------------------------------------------------------------------------------
-# pre-calculation (outside the function so not repeated) ----
-#Y = as.sociomatrix(movie_net_watch_full, ignore.eval = F, attrname = "ratings_diff")
-Y = t(Cmatrix)
-#Y = M #note row variance much higher than col variance
-c = 1
-Yw = Y + c # for Y-based weights, add a constant to Y if it has zeros so no 0 weights
-diag(Yw) = 0 # assume no self-loops
-lgamma.constant = sum(lgamma(as.vector(Y+1))) # constant for factorial term
-W = Yw*(log(Yw)-1) + 5 #add so all weights > 0  
-diag(W) = 0
-Yw_row = rowSums(Yw)
-Yw_col = colSums(Yw)
-W_row = rowSums(W)
-W_col = colSums(W)
-
-N = n = nrow(Y); d = 2
-B = rnorm(1)
-Z = rnorm(N*d)
-Z_dist = dist2(Y)
-#Z = cmdscale(Z_dist, k = d)
-#Z = Z/max(abs(Z))
-a = rnorm(N)
-b = rnorm(N)
-sigma2_a = 1
-sigma2_b = 1
-sigma2_z = 1
-
-beta.var = 9
-sender.var.df = 3
-receiver.var.df = 3
-Z.var.df = sqrt(N)
-prior.sender.var = 1
-prior.receiver.var = 1
-prior.Z.var = N/8
-
-#----------------------------------------------------------------------------------------------------
-# run the model
-# target for citation with shift(4819, 5061)
-# llik(c(latent.srp2.init.res$mkl, sender.var = latent.srp2.init.res$mcmc.mle$sender.var, receiver.var = latent.srp2.init.res$mcmc.mle$receiver.var, Z.var = latent.srp2.init.res$mcmc.mle$Z.var), Y = Y, est= "MAP")
-# target for movie_acw with shift (37332.87, 37998.51)
-# target for movie_watch based on mle (-565856.5, -569542.9)
-# llik(c(latent_acw2_res$mkl, sender.var = latent_acw2_res$mcmc.mle$sender.var, receiver.var = latent_acw2_res$mcmc.mle$receiver.var, Z.var = latent_acw2_res$mcmc.mle$Z.var), Y = Y, est= "MAP")
-# llik(latent.watch.mle$start, Y = Y, est = "Y")
-#---------------------------------------------------------------------------------------------------
-# est Y for just maximizing graph likelihood, MAP for MAP, MAPe for map that uses exact updates of variance 
-# paramters conditioned on B,Z,a,b
-# For MAPe note variance parameters aren't entered in optim or returned, but calculated in llik2 (and gradient func)
-# ---------------------------------------------------------------------------------------------------
-# Y ------------------------------------------------------------------------------------------------
-#  exact ####
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b), 
-                   fn = llik2, Y = Y, d = 2, est = "Y", 
-                   gr = llik_gr,
-                   method = "L-BFGS-B",
-                   lower = c(rep(-50, 1 + N*(d+2))),
-                   control=list(maxit = 100, fnscale = -1)) #<- need high enough maxit
-(Sys.time() - t1)
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts[1]
-theta.bfgs$convergence
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-
-#  approx - only works with gr function #### 
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b), 
-                   fn = llik_hat2, Y = Y, d = 2, est = "Y", r = .25, margin = "none",
-                   gr = llik_gr_hat,
-                   method = "L-BFGS-B",
-                   lower = c(rep(-5000, 1 + N*(d+2))),
-                   control=list(maxit = 100, fnscale = -1)
-                   )
-Sys.time() - t1
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts[1]
-theta.bfgs$convergence
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-
-#plots
-model = latent.watch.mle$mle #latent_acw2$mkl #latent.srp2.init.r$mkl #
-col1 = as.numeric(as.factor(movie_net_acw_full%v%"genres"))#col2
-plot(model$receiver - model$sender,
-     theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)], cex = log(colSums(Y)+2)/5)
-cor(model$receiver - model$sender,
-     theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)])
-plot(matrix(theta.bfgs$par[2:(1+d*n)], ncol = d, nrow = n), col = col1, pch = 16, cex = 2)
-
-
-# ---------------------------------------------------------------------------------------------------
-# MAP ------------------------------------------------------------------------------------------------
-#  exact ####
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b, sigma2_a, sigma2_b, sigma2_z), 
-                   fn = llik2, Y = Y, d = 2, est = "MAP", 
-                   gr = llik_gr, 
-                   method = "L-BFGS-B",
-                   lower = c(-50, rep(-Inf, N*(d+2)), 0, 0, 0), #-Inf lb on B -> error if too neg
-                   control=list(maxit = 2000, fnscale = -1)) 
-(Sys.time() - t1)
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts[1]
-theta.bfgs$convergence #ERROR
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "MAP")
-
-#plots
-plot(model$receiver - model$sender,
-     theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)])
-cor(model$receiver - model$sender,
-    theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)])
-plot(matrix(theta.bfgs$par[2:(1+d*n)], ncol = d, nrow = n), col = col1, pch = 16, cex = 2)
-
-# ---------------------------------------------------------------------------------------------------
-# MAPe (better, i think) ------------------------------------------------------------------------------------------------
-#  exact ####
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b), 
-                   fn = llik2, Y = Y, d = 2, est = "MAPe", 
-                   gr = llik_gr, 
-                   method = "L-BFGS-B",
-                   lower = c(rep(-Inf, 1 + N*(d+2))),
-                   control=list(maxit = 100, fnscale = -1))
-Sys.time() - t1
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts
-theta.bfgs$convergence
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "MAPe")
-
-#  approx ####
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b), 
-                   fn = llik_hat2, Y = Y, d = 2, est = "MAPe", r = .25, margin = "none",
-                   gr = llik_gr_hat, 
-                   method = "L-BFGS-B",
-                   lower = c(rep(-Inf, 1 + N*(d+2))),
-                   control=list(maxit = 2000, fnscale = -1))
-Sys.time() - t1
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts
-theta.bfgs$convergence
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "MAPe")
-
-#check
-B1 = theta.bfgs$par[1]
-Z1 = matrix(theta.bfgs$par[2:(1+d*n)], ncol = d, nrow = n)
-a1 = theta.bfgs$par[(2+d*n):(n+1+d*n)]
-b1 = theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)]
-sigma2_a = (sum(a1^2) + sender.var.df*prior.sender.var^2) / (n + 2 + prior.sender.var)
-sigma2_b = (sum(b1^2) + receiver.var.df*prior.receiver.var^2) / (n + 2 + prior.receiver.var)
-sigma2_z = (sum(Z1^2) + Z.var.df*prior.Z.var^2) / (n*d + 2 + prior.Z.var)
-
-#plots
-plot(model$receiver - model$sender, b1 - a1, cex = log(rowSums(Y)+5)/5)
-cor(model$receiver - model$sender, b1 - a1)
-plot(Z1, col = col1, pch = 16, cex = 2)
-
-
 # ------------------------------------------------------------------------------------------------
-# 4. Multi-dimensional sender and receiver
-# Y ------------------------------------------------------------------------------------------------
-#  exact ####
-t1 = Sys.time()
-theta.bfgs = optim(par = c(B, Z, a, b), 
-                   fn = llik2, Y = Y, d = 2, est = "Y", 
-                   #gr = llik_gr,
-                   method = "L-BFGS-B",
-                   lower = c(rep(-50, 1 + N*(d+2))),
-                   control=list(maxit = 1000, fnscale = -1)) #<- need high enough maxit
-(Sys.time() - t1)
-(Sys.time() - t1)/theta.bfgs$counts[1]
-theta.bfgs$counts[1]
-theta.bfgs$convergence
-llik2(theta.bfgs$par, Y = Y, d = 2, est= "Y")
-
-theta = theta.bfgs$par
-nparam = length(theta)
-a = theta[(2+d*n): (1+d*n/2 + (nparam-1)/2) ]
-b = theta[(2+d*n/2 + (nparam-1)/2) : nparam]
-plot(b[1:47], b[48:94], type = "n")
-text(b[1:47], b[48:94], rownames(Cmatrix), cex = 1, col = col2)
-#plot(a[1:47], a[48:94], type = "n")
-plot(a[1:47], a[48:94], type = "n")
-text(a[1:47], a[48:94], rownames(Cmatrix), col= col2, cex = .7)
-plot(theta[2:48], theta[49:95], type = "n")
-text(theta[2:48], theta[49:95], rownames(Cmatrix), col= col2, cex = 1)
-
-plot(b[1:47] - a[1:47], latent.srp2.init.r$mkl$receiver - latent.srp2.init.r$mkl$sender)
-plot(b[48:94] - a[48:94], latent.srp2.init.r$mkl$receiver - latent.srp2.init.r$mkl$sender)
-plot(b[1:47]+b[48:94] - a[1:47] - a[48:94], latent.srp2.init.r$mkl$receiver - latent.srp2.init.r$mkl$sender)
-
-#plots
-model = latent.watch.mle$mle #latent_acw2$mkl #latent.srp2.init.r$mkl #
-col1 = as.numeric(as.factor(movie_net_acw_full%v%"genres"))#col2
-plot(model$receiver - model$sender,
-     theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)], cex = log(colSums(Y)+2)/5)
-cor(model$receiver - model$sender,
-    theta.bfgs$par[(n+2+d*n):(2*n+1+d*n)] - theta.bfgs$par[(2+d*n):(n+1+d*n)])
-plot(matrix(theta.bfgs$par[2:(1+d*n)], ncol = d, nrow = n), col = col1, pch = 16, cex = 2)
-
-
-
-# ------------------------------------------------------------------------------------------------
-# 5. NOTES --------------------------------------------------------------------------------------------
+# 4. NOTES --------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------
 theta.sann = optim(c(B, Z, a, b),
                    Y = Y, d = 2, llik_hat2, method = "SANN",
